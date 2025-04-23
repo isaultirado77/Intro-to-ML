0027b1e4050345ee82ee69551b57049f7f4b831f
Isaul <tiradolisaul@gmail.com>
2025-04-22 19:48:50 -0700

    mooc-fi solved p03: 1-2

--- mooc-fi/mooc-data-analysis-with-python-2024-2025/part03-e01_column_comparison/src/column_comparison.py
+++ mooc-fi/mooc-data-analysis-with-python-2024-2025/part03-e01_column_comparison/src/column_comparison.py
@@ -2,11 +2,18 @@
 
 import numpy as np
 
-def column_comparison(a):
-    return np.array([])
+def column_comparison(arr: np.ndarray):
+    mask = arr[:, 1] > arr[:, -2] 
+    return arr[mask]
     
 def main():
-    pass
+    a = np.array( 
+    [[8, 9, 3, 8, 8],
+    [0, 5, 3, 9, 9],
+    [5, 7, 6, 0, 4],
+    [7, 8, 1, 6, 2],
+    [2, 1, 3, 5, 8]])
+    print(column_comparison(a))
 
 if __name__ == "__main__":
     main()
--- mooc-fi/mooc-data-analysis-with-python-2024-2025/part03-e02_first_half_second_half/src/first_half_second_half.py
+++ mooc-fi/mooc-data-analysis-with-python-2024-2025/part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -2,11 +2,16 @@
 
 import numpy as np
 
-def first_half_second_half(a):
-    return np.array([])
+def first_half_second_half(a: np.ndarray):
+    _, m = a.shape
+    m = m // 2
+    mask = np.sum(a[:, :m], axis=1) > np.sum(a[:, m: ], axis=1)
+    return a[mask]
 
 def main():
-    pass
+    a = np.array([[1, 3, 4, 2],
+              [2, 2, 1, 2]])
+    print( first_half_second_half(a) )
 
 if __name__ == "__main__":
     main()
